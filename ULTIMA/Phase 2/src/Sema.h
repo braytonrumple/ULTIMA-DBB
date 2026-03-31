37bb869ce8854e7c911e947544907da206419942
domin <dmarica@iu.edu>
2026-03-31 12:04:53 -0400

    Implemented Phase 2 with Output

---
+++ ULTIMA/Phase 2/src/Sema.h
@@ -0,0 +1,23 @@
+#ifndef SEMA_H
+#define SEMA_H
+
+#include <queue>
+#include <iostream>
+
+// Forward declaration - no need to include Sched.h
+struct TCB;
+
+class Semaphore {
+private:
+    char resource_name[64];
+    int sema_value;
+    std::queue<TCB*> sema_queue;  // TCB* only needs forward declaration
+
+public:
+    Semaphore(const char* name);
+    void down(TCB* task);
+    void up();
+    void dump(int level);
+};
+
+#endif
